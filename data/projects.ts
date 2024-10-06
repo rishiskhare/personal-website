7af1b5bb2586ef395c42d49f09f79455224fa2a1
Rishi Khare <rishiskhare@gmail.com>
2024-10-05 17:57:36 -0700

    Edited text on the website

--- data/projects.ts
+++ data/projects.ts
@@ -13,12 +13,13 @@ export const projects: Item[] = [
   },
   {
     title: 'CoursePath',
-    description: 'Built and hosted course selection planning website for high school district.',
+    description: 'Built and hosted course selection planning website for my high school district (FUHSD)',
     details: [
       'Developed a full-stack web application using Node.js and MySQL',
       'Implemented user authentication and personalized course recommendations',
       'Created an intuitive interface for students to plan their academic path',
-      'Integrated with the school district\'s curriculum database for up-to-date course information'
+      'Integrated with the school district\'s curriculum database for up-to-date course information',
+      'Pitched and marketed the product to families of the school district, my school\'s principal, and guidance counselors'
     ]
   },
 ]
