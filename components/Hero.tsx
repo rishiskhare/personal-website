2230e3015f1f9ad2c9712f201f1d189cf7f9eba4
Rishi Khare <rishiskhare@gmail.com>
2024-10-05 18:40:27 -0700

    Resolved dependency issues in Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Image from 'next/image';
 
 interface HeroProps {
